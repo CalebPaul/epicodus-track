8e4ea8cfb16931f2401a614ff0bc46a27e0c4148
Caleb Paul <calebpaul21@gmail.com>
2016-08-12 13:32:35 -0700

    add text for questions 4 and 5, and complete a section of the js branching statements

--- js/scripts.js
+++ js/scripts.js
@@ -10,27 +10,25 @@ $( document ).ready(function() {
     var q1 = $("input:radio[name=question1]:checked").val();
     if ( q1 === "bigbiz" ) { csharp ++; jav ++; }
       else if (q1 === "startup") { ruby ++; }
-      else if (q1 === "none") { }
 
     var q2 = $("input:radio[name=question2]:checked").val();
     if ( q2 === "design" ) { design ++; }
-      else if (q2 === "mobile") { jav ++; }
+      else if (q2 === "mobile") { jav +=2; }
       else if (q2 === "rails") { ruby ++; }
 
     var q3 = $("input:radio[name=question3]:checked").val();
     if ( q3 === "design" ) { design ++; }
       else if (q3 === "csharp") { csharp ++; }
-      else if (q3 === "ruby") { ruby ++; }
+      else if (q3 === "ruby") { ruby +=2; }
 
-    // var q4 = $("input:radio[name=question4]:checked").val();
-    // if ( q4 === "" ) {  ++; }
-    //   else if (q4 === "") {  ++; }
-    //   else if (q4 === "") {  ++; }
-    //
-    // var q5 = $("input:radio[name=question5]:checked").val();
-    // if ( q5 === "" ) {  ++; }
-    //   else if (q5 === "") {  ++; }
-    //   else if (q5 === "") {  ++; }
+    var q4 = $("input:radio[name=question4]:checked").val();
+    if ( q4 === "csharp" ) { csharp ++; }
+      else if (q4 === "design") { design ++; }
+      else if (q4 === "") { jav ++; }
+
+    var q5 = $("input:radio[name=question5]:checked").val();
+    if ( q5 === "design" ) { design ++; }
+      else if (q5 === "backend") { csharp ++; jav ++; }
 
   console.log("user selected: " + q1);
   console.log("c#: " + csharp);
