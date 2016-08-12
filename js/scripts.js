bd78f22ca2cee8414b937f71dd98a6e0b97dc246
Caleb Paul <calebpaul21@gmail.com>
2016-08-12 11:01:41 -0700

    add some back end js with debugging code written in, and add text for question 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,30 @@
+//Math Logic
+var csharp = 0;
+var jav = 0;
+var php = 0;
+var ruby = 0;
+var design = 0;
+
 $( document ).ready(function() {
+  $("#quiz").submit(function(event) {
 
+    var q1 = $("input:radio[name=question1]:checked").val();
 
+    if ( q1 === "bigbiz" ) {
+      csharp ++;
+      jav ++;
+    } else if (q1 === "startup" ) {
+      ruby ++;
+      design ++;
+    } else if (q1 === "none") {}
 
+//  alert("clicktest");
+  console.log("user selected: " + q1);
+  console.log("c#: " + csharp);
+  console.log("java: " + jav);
+  console.log("php: " + php);
+  console.log("ruby: " + ruby);
+  console.log("design: " + design);
+  event.preventDefault();
+  });
 });
